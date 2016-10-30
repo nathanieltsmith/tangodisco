a6a8001775032166012f9db5563edd39d16122c1
Nathaniel Smith <nathanieltsmith@gmail.com>
2016-10-30 11:39:25 -0600

    initial setup and framework

--- server/routes.js
+++ server/routes.js
@@ -11,16 +11,33 @@ module.exports = function (app, passport) {
     failureFlash: true // allow flash messages
   }))
 
-  app.get('/logout', function (req, res) {
+  app.get('/logout', (req, res) => {
     req.logout()
     res.redirect('/api/user')
   })
 
-  app.get('/api/user', function (req, res) {
+  app.get('/api/user', (req, res) => {
     if (req.user) {
       res.json({ loggedIn: true, user: req.user, flash: req.flash()})
     } else { // We have logged out...
       res.json({loggedIn: false, flash: req.flash()})
     }
   })
+
+  app.route('/api/recordings')
+    .get((req, res) => {
+      res.json({description: 'query the recordings'})
+    })
+    .post(isLoggedIn, (req, res) => {
+      res.json({description: 'create a recording'})
+    })
+}
+
+function isLoggedIn (req, res, next) {
+  // if user is authenticated in the session, carry on 
+  if (req.isAuthenticated())
+    return next()
+
+  // if they aren't redirect them to the home page
+  res.type('json').status(401).send({error: 'User is not logged in.'})
 }
